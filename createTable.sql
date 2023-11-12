18304d2a974ce1bfdf718129aff91b0b0c9d63e1
Minh930906 <minh930906@gmail.com>
2023-11-13 00:04:39 +0100

    sql script modified

--- createTable.sql
+++ createTable.sql
@@ -21,16 +21,16 @@ CREATE TABLE IF NOT EXISTS marketplace (
 
 CREATE TABLE IF NOT EXISTS listing (
     id UUID PRIMARY KEY,
-    title VARCHAR(255),
-    description TEXT,
-    location_id UUID REFERENCES location(id),
-    listing_price DOUBLE PRECISION,
-    currency VARCHAR(10),
-    quantity INTEGER,
-    listing_status_id INTEGER REFERENCES listing_status(id),
-    marketplace_id INTEGER REFERENCES marketplace(id),
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    location_id UUID REFERENCES location(id) NOT NULL,
+    listing_price DECIMAL(10, 2) NOT NULL CHECK (listing_price > 0),
+    currency VARCHAR(3) NOT NULL,
+    quantity INTEGER NOT NULL CHECK (quantity > 0),
+    listing_status_id INTEGER REFERENCES listing_status(id) NOT NULL,
+    marketplace_id INTEGER REFERENCES marketplace(id) NOT NULL,
     upload_time TIMESTAMP,
-    owner_email_address VARCHAR(255)
+    owner_email_address VARCHAR(255) NOT NULL
 );
 
 ALTER TABLE listing
